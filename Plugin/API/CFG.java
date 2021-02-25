b8438e24fd6ba10b3d3a3c5f28b70c42f17f50ef
GrupTheMusicBot "Kuebjii <42746052+GrupMusicBot@users.noreply.github.com>
2021-02-24 23:44:39 -0700

    Update CFG.java

--- Plugin/API/CFG.java
+++ Plugin/API/CFG.java
@@ -2,8 +2,8 @@ package com.kuebv.mcapi.mcapi.API;
 
 public class CFG {
 
-    public String HostIP = "203.10.96.26";
-    public String MongoURL = "mongodb+srv://EdenAdmin:j2NmOEmDLb@discordcluster.lxrev.mongodb.net/DiscordBot?retryWrites=true&w=majority";
+    public String HostIP = "";
+    public String MongoURL = "";
     public String MongoDatabase = "MC";
     public String MongoCollection = "API";
 }
